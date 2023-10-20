648c741de7eda145acc8ebf3052d7464a4aac687
Anjalee Kulasinghe <anjaleek@gmail.com>
2023-10-20 11:38:11 +0000

    Change the code to not to take the re-enter letters

--- run.py
+++ run.py
@@ -17,19 +17,29 @@ print(display)
 
 # Initialize the number of lives
 lives = 6
+guessed_letters = []
 
 # Guessing the correct letter
 game_over = False
 
 while not game_over:
     guessed_letter = input("Guess a letter: ").lower()
+    
+    if guessed_letter in guessed_letters:
+        print("You have already guessed this letter.")
+        continue
+    
+    guessed_letters.append(guessed_letter)
+    
+    letter_guessed = False  # Flag to check if the letter was guessed
     for position in range(len(choose_word)):
         letter = choose_word[position].lower()
         if letter == guessed_letter:
             display[position] = choose_word[position]  # Fix the variable name here
+            letter_guessed = True  # Set the flag to True
     print(display)
 
-    if guessed_letter not in choose_word.lower():
+    if not letter_guessed:  # Check if the letter was not guessed
         lives -= 1
         if lives == 0:
             game_over = True
@@ -37,3 +47,4 @@ while not game_over:
         if '_' not in display:
             game_over = True
             print("WOW, you won the game !!!")
+        print(hangman_stages.stages[lives])
