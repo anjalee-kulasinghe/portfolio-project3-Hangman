97d69b2c52a1f297c0dd44a8caee7cdbb407dce8
Anjalee Kulasinghe <anjaleek@gmail.com>
2023-10-20 09:08:07 +0000

    Add the word list

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 import random
-from tkinter import *
-from string import ascii_uppercase
+
+# List of words to choose from
+word_list = ["INTERNET", "COMPUTER", "SOFTWARE", "DATABASE", "INFORMATION", "INFORMATICS", "COMPUTING", "ELECTRONICS", "NETWORK", "SYSTEM", "HARDWARE", "ENGINEERING", "KNOWLEDGE", "MEDIA"]
 
