a2740df159cc7ca6504ad507cf43e19c10a7fa09
Anjalee Kulasinghe <anjaleek@gmail.com>
2023-11-14 19:17:08 +0000

    Add the code to generate the dashes to the selected word.

--- run.py
+++ run.py
@@ -9,15 +9,24 @@ run = True
 while run:
     # initializing a Tkinter window
     root = Tk()
-    root.geometry('905x700') # screen size
+    root.geometry('905x700')
     root.title('HANGMAN')
-    root.config(bg='#E7FFFF') # screen background colour
+    root.config(bg = '#E7FFFF')
     count = 0
     win_count = 0
-    secret_word_label = Label(root, text='', bg="#E7FFFF", font=("arial", 20))
 
-    # choosing a random word
-    index = random.randint(0,853)
+    # choosing the random word
+    index = random.randint(0,58109)
     file = open('words.txt','r') # import the word text file
-    l = file.readlines()
-    selected_word = l[index].strip('\n')
+    list = file.readlines()
+    selected_word = list[index].strip('\n')
+
+    # creating the dashes according to the selected word
+    dashes_labels = []
+
+    x_position = 250
+    for i in range(len(selected_word)):
+        x_position += 60
+        dash_label = Label(root, text="_", bg="#E7FFFF", font=("arial", 40))
+        dash_label.place(x=x_position, y=450)
+        dashes_labels.append(dash_label)
