45c08efc0000525d4648a8156e486a31af861e06
Anjalee Kulasinghe <anjaleek@gmail.com>
2023-11-14 20:30:42 +0000

    Add the code to bring the image files.

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ run = True
 while run:
     # initializing a Tkinter window
     root = Tk()
-    root.geometry('905x700')
+    root.geometry('950x725')
     root.title('HANGMAN')
     root.config(bg = '#E7FFFF')
     count = 0
@@ -18,8 +18,8 @@ while run:
     # choosing the random word
     index = random.randint(0,58109)
     file = open('words.txt','r') # import the word text file
-    list = file.readlines()
-    selected_word = list[index].strip('\n')
+    word_list = file.readlines()
+    selected_word = word_list[index].strip('\n')
 
     # creating the dashes according to the selected word
     dashes_labels = []
@@ -30,3 +30,30 @@ while run:
         dash_label = Label(root, text="_", bg="#E7FFFF", font=("arial", 40))
         dash_label.place(x=x_position, y=450)
         dashes_labels.append(dash_label)
+    
+    # Create a dictionary to store PhotoImage objects
+    image_dict = {}
+    for let in 'abcdefghijklmnopqrstuvwxyz':
+        image_dict[let] = PhotoImage(file=f"{let}.png")
+    
+    # Hangman images
+    hangman_image = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7']
+    hangman_images = [PhotoImage(file=f"{hangman}.png") for hangman in hangman_image]
+    hangman_label = Label(root, bg="#E7FFFF", image=hangman_images[count])
+    hangman_label.place(x=300, y=-50)
+
+    # Letters placement
+    buttons = []
+    button_height = 70  # Adjust this value based on your layout
+    for i, letter in enumerate('abcdefghijklmnopqrstuvwxyz'):
+        row = i // 13  # Change the number 13 based on the number of buttons per row you want
+        col = i % 13
+        button = Button(root, bd=0, command=lambda l=letter, idx=i + 1: check(l, idx), bg="#E7FFFF",
+                        activebackground="#E7FFFF", font=10, image=image_dict[letter])
+        button.place(x=col * 70, y=row * button_height + 595)
+        buttons.append(button)
+
+    # Exit button
+    exit_image = PhotoImage(file='exit.png')
+    exit_button = Button(root, bd=0, command=close, bg="#E7FFFF", activebackground="#E7FFFF", font=10, image=exit_image)
+    exit_button.place(x=770, y=10)
